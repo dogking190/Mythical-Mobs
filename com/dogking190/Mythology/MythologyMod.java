ab58739f89f1df35683919c5efcc140867625b4a
KeeperofMee <kim.thysell@live.se>
2014-04-16 18:40:48 +0200

    Added WorkingTools.

--- com/dogking190/Mythology/MythologyMod.java
+++ com/dogking190/Mythology/MythologyMod.java
@@ -6,7 +6,9 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityList;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemTool;
 import net.minecraftforge.common.util.EnumHelper;
 
 import com.dogking190.Mythology.mobs.EntityCentaur;
@@ -20,6 +22,11 @@ import com.dogking190.Mythology.modBlocks.blockPlatinumOre;
 import com.dogking190.Mythology.modBlocks.blockRubyOre;
 import com.dogking190.Mythology.modBlocks.blockSapphireOre;
 import com.dogking190.Mythology.modBlocks.blockSilverOre;
+import com.dogking190.Mythology.modItems.itemAxe;
+import com.dogking190.Mythology.modItems.itemHoe;
+import com.dogking190.Mythology.modItems.itemHoeIG;
+import com.dogking190.Mythology.modItems.itemPickaxe;
+import com.dogking190.Mythology.modItems.itemShovel;
 import com.dogking190.Mythology.proxy.CommonProxy;
 
 import cpw.mods.fml.common.Mod;
@@ -71,7 +78,7 @@ public class MythologyMod {
 
 	
 		//Setting new toolmaterials
-			static ToolMaterial toolBronze = EnumHelper.addToolMaterial("bronze", 2, 341, 5.0F, 2.7F, 14);
+			static ToolMaterial toolBronze = EnumHelper.addToolMaterial("bronze", 2, 341, 5.0F, 2F, 14);
 			static ToolMaterial toolCelestialBronze = EnumHelper.addToolMaterial("celestialbronze", 2, 453, 6.4F, 3F, 10);
 			static ToolMaterial toolImperialGold = EnumHelper.addToolMaterial("imperialgold", 2, 532, 8F, 5F, 10);
 			static ToolMaterial toolSilver = EnumHelper.addToolMaterial("silver", 2, 332, 5.8F, 4F, 14);
@@ -81,7 +88,22 @@ public class MythologyMod {
 		public static Item swordCelestialBronze = new ItemSword(toolCelestialBronze).setUnlocalizedName("swordCelestialBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "swordCelestialBronze");
 		public static Item swordImperialGold = new ItemSword(toolImperialGold).setUnlocalizedName("swordImperialGold").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "swordImperialGold");
 		public static Item swordSilver = new ItemSword(toolSilver).setUnlocalizedName("swordSilver").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "swordSilver");
-	 
+		public static Item pickaxeBronze = new itemPickaxe(toolBronze).setUnlocalizedName("pickaxeBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "pickaxeBronze");
+		public static Item pickaxeCelestialBronze = new itemPickaxe(toolCelestialBronze).setUnlocalizedName("pickaxeCelestialBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "pickaxeCelestialBronze");
+		public static Item pickaxeImperialGold = new itemPickaxe(toolImperialGold).setUnlocalizedName("pickaxeImperialGold").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "pickaxeImperialGold");
+		public static Item pickaxeSilver = new itemPickaxe(toolSilver).setUnlocalizedName("pickaxeSilver").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "pickaxeSilver");
+		public static Item shovelBronze = new itemShovel(toolBronze).setUnlocalizedName("shovelBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "shovelBronze");
+		public static Item shovelCelestialBronze = new itemShovel(toolCelestialBronze).setUnlocalizedName("shovelCelestialBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "shovelCelestialBronze");
+		public static Item shovelImperialGold = new itemShovel(toolImperialGold).setUnlocalizedName("shovelImperialGold").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "shovelImperialGold");
+		public static Item shovelSilver = new itemShovel(toolSilver).setUnlocalizedName("shovelSilver").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "shovelSilver");
+		public static Item axeBronze = new itemAxe(toolBronze).setUnlocalizedName("axeBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "axeBronze");
+		public static Item axeCelestialBronze = new itemAxe(toolCelestialBronze).setUnlocalizedName("axeCelestialBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "axeCelestialBronze");
+		public static Item axeImperialGold = new itemAxe(toolImperialGold).setUnlocalizedName("axeImperialGold").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "axeImperialGold");
+		public static Item axeSilver = new itemAxe(toolSilver).setUnlocalizedName("axeSilver").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "axeSilver");
+		public static Item hoeBronze = new itemHoe(toolBronze).setUnlocalizedName("hoeBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "hoeBronze");
+		public static Item hoeCelestialBronze = new itemHoe(toolCelestialBronze).setUnlocalizedName("hoeCelestialBronze").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "hoeCelestialBronze");
+		public static Item hoeImperialGold = new itemHoeIG(toolImperialGold).setUnlocalizedName("hoeImperialGold").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "hoeImperialGold");
+		public static Item hoeSilver = new itemHoe(toolSilver).setUnlocalizedName("hoeSilver").setCreativeTab(tabMythical).setTextureName(MythologyMod.modid + ":" + "hoeSilver");
  
 	
 
@@ -120,6 +142,23 @@ public class MythologyMod {
     	GameRegistry.registerItem(swordCelestialBronze, "swordCelestialBronze"); 
     	GameRegistry.registerItem(swordImperialGold, "swordImperialGold"); 
     	GameRegistry.registerItem(swordSilver, "swordSilver"); 
+    	GameRegistry.registerItem(pickaxeBronze, "pickaxeBronze"); 
+    	GameRegistry.registerItem(pickaxeCelestialBronze, "CelestialBronze"); 
+    	GameRegistry.registerItem(pickaxeImperialGold, "pickaxeImperialGold"); 
+    	GameRegistry.registerItem(pickaxeSilver, "pickaxeSilver"); 
+    	GameRegistry.registerItem(shovelBronze, "shovelBronze"); 
+    	GameRegistry.registerItem(shovelCelestialBronze, "shovelCelestialBronze"); 
+    	GameRegistry.registerItem(shovelImperialGold, "shovelImperialGold"); 
+    	GameRegistry.registerItem(shovelSilver, "shovelSilver"); 
+    	GameRegistry.registerItem(axeBronze, "axeBronze"); 
+    	GameRegistry.registerItem(axeCelestialBronze, "axeCelestialBronze"); 
+    	GameRegistry.registerItem(axeImperialGold, "axeImperialGold"); 
+    	GameRegistry.registerItem(axeSilver, "axeSilver"); 
+    	GameRegistry.registerItem(hoeBronze, "hoeBronze"); 
+    	GameRegistry.registerItem(hoeCelestialBronze, "hoeCelestialBronze"); 
+    	GameRegistry.registerItem(hoeImperialGold, "hoeImperialGold"); 
+    	GameRegistry.registerItem(hoeSilver, "hoeSilver"); 
+
 
 		
 		//World Generator
